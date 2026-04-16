84d506e4b097aafdecad561c88fc32d86c296000
Chrystiam Nascimento Junior <chrystiamjr@gmail.com>
2026-04-16 12:53:03 -0300

    style(dart): format generated GUI provider

--- gui/lib/src/runtime/run/desktop_run_controller_provider.g.dart
+++ gui/lib/src/runtime/run/desktop_run_controller_provider.g.dart
@@ -6,37 +6,29 @@ part of 'desktop_run_controller_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$desktopRunControllerFactoryHash() =>
-    r'50575786b3a2f543d563a49ec7e0dc7691de9cc2';
+String _$desktopRunControllerFactoryHash() => r'50575786b3a2f543d563a49ec7e0dc7691de9cc2';
 
 /// See also [desktopRunControllerFactory].
 @ProviderFor(desktopRunControllerFactory)
-final desktopRunControllerFactoryProvider =
-    Provider<DesktopRunControllerFactory>.internal(
-      desktopRunControllerFactory,
-      name: r'desktopRunControllerFactoryProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$desktopRunControllerFactoryHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
+final desktopRunControllerFactoryProvider = Provider<DesktopRunControllerFactory>.internal(
+  desktopRunControllerFactory,
+  name: r'desktopRunControllerFactoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$desktopRunControllerFactoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef DesktopRunControllerFactoryRef =
-    ProviderRef<DesktopRunControllerFactory>;
-String _$desktopRunControllerHash() =>
-    r'57510703c66b85214503821efe46e6b6690dc3f1';
+typedef DesktopRunControllerFactoryRef = ProviderRef<DesktopRunControllerFactory>;
+String _$desktopRunControllerHash() => r'57510703c66b85214503821efe46e6b6690dc3f1';
 
 /// See also [desktopRunController].
 @ProviderFor(desktopRunController)
 final desktopRunControllerProvider = Provider<DesktopRunController>.internal(
   desktopRunController,
   name: r'desktopRunControllerProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$desktopRunControllerHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$desktopRunControllerHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
@@ -44,17 +36,14 @@ final desktopRunControllerProvider = Provider<DesktopRunController>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef DesktopRunControllerRef = ProviderRef<DesktopRunController>;
-String _$desktopRunSnapshotsHash() =>
-    r'45c00f79acb4d8f659bd913e14a68019e4048a65';
+String _$desktopRunSnapshotsHash() => r'45c00f79acb4d8f659bd913e14a68019e4048a65';
 
 /// See also [desktopRunSnapshots].
 @ProviderFor(desktopRunSnapshots)
 final desktopRunSnapshotsProvider = StreamProvider<DesktopRunSnapshot>.internal(
   desktopRunSnapshots,
   name: r'desktopRunSnapshotsProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$desktopRunSnapshotsHash,
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product') ? null : _$desktopRunSnapshotsHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
